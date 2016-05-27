b9124cd89e3ecbd65a177798d1c49510064a9497
Josh English <josh@joshenglish.com>
2016-05-27 09:23:58 -0500

    Adding BadSum and GoodSum

---
+++ bad_sum/bad_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// this is the improper way to do this
+
+class BadSum {
+    static void Main() {
+        string s, t, sum;
+        Console.Write ("Enter an integer: ");
+        s = Console.ReadLine ();
+        Console.Write ("Enter another integer: ");
+        t = Console.ReadLine ();
+        sum = s + t;
+        Console.WriteLine ("They add up to " + sum);
+    }
+}
