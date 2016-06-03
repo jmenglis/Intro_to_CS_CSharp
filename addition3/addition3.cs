93870c38138e7adbc5ab1eb355d4fc0c3e0d1f2c
Josh English <josh@joshenglish.com>
2016-06-03 14:36:58 -0500

    Adding UI and UIF libraries for additional work

---
+++ addition3/addition3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntroCS {
+    class Addition3 {
+        static string SumProblemString(int x, int y) {
+            int sum = x + y;
+            string sentence = "The sum of " + x + " and " + y + " is " + sum + ".";
+            return sentence;
+        }
+        public static void Main() {
+            Console.WriteLine(SumProblemString(2, 3));
+            Console.WriteLine(SumProblemString(12345, 53579));
+            int a = UIF.PromptInt("Enter an integer: ");
+            int b = UIF.PromptInt("Enter another integer: ");
+            Console.WriteLine(SumProblemString(a, b));
+        }
+    }
+}
