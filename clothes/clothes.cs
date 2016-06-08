fe6e0a2845405b1319d3d2acd7513c29d38cd1a2
Josh English <josh@joshenglish.com>
2016-06-07 21:06:26 -0500

    Adding Clothes project

---
+++ clothes/clothes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace IntroCS {
+    class Clothes {
+        static void Main() {
+            double temperature = UIF.PromptDouble("What is the temperature? ");
+            if (temperature > 70) {
+                Console.WriteLine("Wear shorts.");
+            } else {
+                Console.WriteLine("Wear long pants");
+            }
+            Console.WriteLine("Get some exercise outside.");
+        }
+    }
+}
