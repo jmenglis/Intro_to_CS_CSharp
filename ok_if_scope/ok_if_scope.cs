6b0e0bd55b7933d6beb73efdcb32bd63fc21a69d
Josh English <josh@joshenglish.com>
2016-06-08 14:25:31 -0500

    Adding ok_if_scope to project

---
+++ ok_if_scope/ok_if_scope.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IntroCS {
+    class OkIfScope {
+        static void Main() {
+            Console.WriteLine(OkScope(5));
+            Console.WriteLine(OkScope2(5));
+        }
+
+        static int OkScope(int x) {
+            int val;
+
+            if (x<100) {
+                val = x + 2;
+            } else {
+                val = x - 2;
+            }
+            return val;
+        }
+
+        static int OkScope2(int x) {
+            int val = 0;
+
+            if (x < 100) {
+                val = x + 2; 
+            }
+            if (x >= 100) {
+                val = x - 2;
+            }
+            return val;
+        }
+    }
+}
