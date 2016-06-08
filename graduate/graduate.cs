ea06c5467f0706fc56b9693aa5b3e63d0a036c8f
Josh English <josh@joshenglish.com>
2016-06-08 16:43:52 -0500

    Graduate Project for CSHARP

---
+++ graduate/graduate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntroCS {
+    class Graduate {
+        static void checkCredits(int credits) {
+            if (credits >= 120) {
+                Console.WriteLine("Congratulations: You have enough credits to graduate! ");
+            } else {
+                Console.WriteLine("Sorry: You do not have enough credits to graduate.");
+            }
+        }
+        static void Main() {
+            int credits = UIF.PromptInt("Please enter your current credits: ");
+            checkCredits(credits);
+        }
+    }
+}
