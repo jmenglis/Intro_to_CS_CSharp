1c9b81c267529e199ec6087a3783613ca0a8d686
Josh English <josh@joshenglish.com>
2016-06-08 17:02:00 -0500

    Grade project for CSHARP

---
+++ grade/grade.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IntroCS {
+    class Grade {
+        static char letterGrade(double score) {
+            char letter;
+            if (score >= 90) {
+                letter = 'A';
+            } else if (score >= 80) {
+                letter = 'B';
+            } else if (score >= 70) {
+                letter = 'C';
+            } else if (score >= 60) {
+                letter = 'D';
+            } else {
+                letter = 'F';
+            }
+            return letter;
+        }
+        static void Main() {
+            double g = UIF.PromptDouble("Enter a numerical grade: ");
+            Console.WriteLine("Your letter grade is {0}", letterGrade(g));
+        }
+    }
+}
