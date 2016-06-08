11d2d1c56265025f9a8fc04bb5f1916d87ecae42
Josh English <josh@joshenglish.com>
2016-06-07 20:47:41 -0500

    Adding Suitcase project

---
+++ suitcase/suitcase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IntroCS {
+    class Suitcase {
+        static void Main() {
+            double weight = UIF.PromptDouble("How many pounds does your suitcase weigh? ");
+            if (weight > 50) {
+                Console.WriteLine("There is a $25 charge for luggage that heavy.");
+            }
+            Console.WriteLine("Thank you for your business");
+        }
+    }
+}
