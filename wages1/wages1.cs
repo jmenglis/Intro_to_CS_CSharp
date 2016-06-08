288ca98a7897c166dd408e48373486d57ff6c85c
Josh English <josh@joshenglish.com>
2016-06-08 14:51:07 -0500

    Adding project wages and wages2

---
+++ wages1/wages1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace IntroCS {
+    class Wages {
+        /// Return the total weekly wages for a working
+        /// totalHours with a given regular hourlyWage
+        /// Include overtime for hours over 40.
+        static double CalcWeekWages(double totalHours, double hourlyWage) {
+            double totalWages;
+            if (totalHours <= 40) {
+                totalWages = totalHours * hourlyWage;
+            } else {
+                double overTime = totalHours - 40;
+                totalWages = hourlyWage * 40 + (overTime * (hourlyWage * 1.5));
+            }
+            return totalWages;
+        }
+        static void Main() {
+            double hours = UIF.PromptDouble("Enter hours worked: ");
+            double wage = UIF.PromptDouble("Enter dollars paid per hour: ");
+            double total = CalcWeekWages(hours, wage);
+            Console.WriteLine("Wages for {0} hours at {1:F2} per hour are {2:F2}.", hours, wage, total);
+        }
+    }
+}
