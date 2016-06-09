ce7869f4e41599d26e36b468a8c65b75a49d920a
Josh English <josh@joshenglish.com>
2016-06-08 19:37:46 -0500

    Adding the sign of numbers

---
+++ sign/sign.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IntroCS {
+    class Sign {
+        static string numberCat(double num) {
+            string category;
+            if (num > 0) {
+                category = "positive";
+            } else if (num < 0) {
+                category = "negative";
+            } else {
+                category = "zero";
+            }
+            return category;
+        }
+        static void Main() {
+            double number = UIF.PromptDouble("Please enter a number: ");
+            Console.WriteLine("{0} is {1}", number, numberCat(number));
+        }
+    }
+}
